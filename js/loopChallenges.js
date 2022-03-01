b85ef018bab41380ce890ee4ac612919d23a51e5
Mustafa Wahid <mustafarwahid@gmail.com>
2022-02-28 20:35:37 -0500

    - initial commit

---
+++ js/loopChallenges.js
@@ -0,0 +1,39 @@
+//ODDS
+for(var i=0;i<20;i++) {
+    if(i%2!==0) {
+      console.log(i)
+    }
+  }
+//Decreasing Multiples of 3
+console.log("Decreasing by 3")
+for(var i=100;i>0;i--){
+    if(i%3===0){
+      console.log(i)
+    }
+  }
+
+//Print the sequence
+console.log("Printing the sequence")
+for(i=4;i>-4;i-=1.5){
+    console.log(i)
+  }
+
+
+
+//Sigma
+sum = 0
+for(var i=1;i<=100;i++){
+    sum = sum+i; 
+    console.log(sum)
+  }
+
+
+//Factorial 
+var num = 12; 
+var fact = 1; 
+for(var i=num;i>=1;i--) {
+  console.log( fact = fact*i)
+}
+
+
+  
